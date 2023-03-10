e156f4b9ed75982602c4c2ac463fb043d91d1010
jump3rhood <mjm13121@gmail.com>
2023-03-10 20:02:31 +0530

    ex 7.5 extend useField with reset functionality

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -83,7 +83,12 @@ const CreateNew = (props) => {
     // redirect to /
     navigate('/')
   }
-
+  const handleReset = (event) => {
+    event.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -100,7 +105,8 @@ const CreateNew = (props) => {
           url for more info
           <input name='info'  {...info} />
         </div>
-        <button>create</button>
+        <button onClick={handleReset}>reset</button>
+        <button type='submit'>create</button>
       </form>
     </div>
   )
